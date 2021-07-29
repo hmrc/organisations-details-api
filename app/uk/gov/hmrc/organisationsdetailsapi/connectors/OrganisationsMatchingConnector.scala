2a8153b5481ec8c4ca8aa7fc39064e5c40437fc0
owengilfellon <owen.gilfellon@opencastsoftware.com>
2021-07-29 11:06:28 +0100

    HODS-175 | Updated OrgsMatchingController error recovery

--- app/uk/gov/hmrc/organisationsdetailsapi/connectors/OrganisationsMatchingConnector.scala
+++ app/uk/gov/hmrc/organisationsdetailsapi/connectors/OrganisationsMatchingConnector.scala
@@ -16,13 +16,15 @@
 
 package uk.gov.hmrc.organisationsdetailsapi.connectors
 
-import uk.gov.hmrc.http.{HeaderCarrier, HttpClient}
+import uk.gov.hmrc.http.{HeaderCarrier, HttpClient, NotFoundException}
 import uk.gov.hmrc.organisationsdetailsapi.domain.OrganisationMatch
 import uk.gov.hmrc.organisationsdetailsapi.domain.OrganisationMatch._
 import uk.gov.hmrc.play.bootstrap.config.ServicesConfig
-
 import java.util.UUID
+
 import javax.inject.Inject
+import uk.gov.hmrc.organisationsdetailsapi.errorhandler.ErrorResponses.MatchNotFoundException
+
 import scala.concurrent.{ExecutionContext, Future}
 
 class OrganisationsMatchingConnector @Inject()(httpClient: HttpClient, servicesConfig: ServicesConfig) {
@@ -30,5 +32,7 @@ class OrganisationsMatchingConnector @Inject()(httpClient: HttpClient, servicesC
   private val serviceUrl = servicesConfig.baseUrl("organisations-matching-api")
 
   def resolve(matchId: UUID)(implicit hc: HeaderCarrier, ec : ExecutionContext): Future[OrganisationMatch] =
-    httpClient.GET[OrganisationMatch](s"$serviceUrl/match-record/$matchId")
+    httpClient.GET[OrganisationMatch](s"$serviceUrl/match-record/$matchId") recover {
+      case _: NotFoundException => throw new MatchNotFoundException
+    }
 }
